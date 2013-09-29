c61ab643bc4ea6284708ace8f1c8b88e8b201fb3
Philip Rader <ifelloffofatree@gmail.com>
2013-09-29 14:36:10 -0700

    Another typo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include_once("./includes/header.php");
 ?>
 <h1>Welcome to Franklin Pierce High School TSA and Robotics website!</h1>
-<p>On this website, you can get gey resources relative to our teams. You can find our sponsors, updates on our season, and competition dates. We hope you cheer us on and support us!</p>
+<p>On this website, you can get resources relative to our teams. You can find our sponsors, updates on our season, and competition dates. We hope you cheer us on and support us!</p>
 <?php
 include_once("./includes/footer.php");
 ?>
